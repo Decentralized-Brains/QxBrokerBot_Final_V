1dc8dff9fec2b28bafdde29771c47ae77b35f0d1
alfazsozib <alfazsozib.com>
2023-11-20 21:18:33 +0600

    footer done

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Contact from "./components/contact/Contact";
 import Faq from "./components/faq/Faq";
 import Hero from "./components/hero/Hero";
 import Pricing from "./components/pricing/Pricing";
-
+import Footer from "./components/global/Footer";
 
 function App() {
   return (
@@ -14,6 +14,7 @@ function App() {
       <Contact />
       <Pricing />
       <Faq />
+      <Footer/>
       
  
     </>
--- src/components/global/Footer.jsx
+++ src/components/global/Footer.jsx
@@ -4,6 +4,12 @@ const Footer = () => {
 
     return (
         <>
+        <div id="footer">
+            <div className="bg-primary-color w-[100%] py-2 flex justify-center  items-center text-white">
+                <p className="font-jost font-semibold">&copy; {new Date().getFullYear()} DecentralizedBrains. All Rights Reserved.</p>
+
+            </div>
+        </div>
         </>
     )
 }
